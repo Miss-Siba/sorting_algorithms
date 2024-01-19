ed03cf793f9c303e53619fd2879c40f001c6198d
Miss-Siba <sibaquma@gmail.com>
2024-01-19 04:39:41 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 /**
  * bubble_sort - sorts array using Bubble sort algorithm.
  * @array: array to be sorted.
@@ -10,9 +11,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temp;
+	bool swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,9 +24,13 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = true;
 
 			}
 		}
-		print_array(array, size);
+		if (swapped)
+		{
+			print_array(array, size);
+		}
 	}
 }
