46455251f535296495ddd83df90f29342312e3f5
Miss-Siba <sibaquma@gmail.com>
2024-01-20 08:54:37 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,20 +22,27 @@ void swap(int *a, int *b)
 
 void selection_sort(int *array, size_t size)
 {
-	size_t min, i, j;
+	size_t  i, j, min;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 
-	for (j = i + 1; j < size; j++)
-	{
-		if (array[j] < array[min])
+		for (j = i + 1; j < size; j++)
 		{
-			min = j;
+
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
 		}
-	}
-	swap(&array[i], &array[min]);
-	print_array(array, size);
+		if (i != min)
+		{
+			swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
 	}
 }
